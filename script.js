b995714af7b1ff7aefb0c5273689f9de0bf15727
JuanOros <juenas@hotmail.com>
2023-03-20 13:49:06 -0300

    Final touches

--- script.js
+++ script.js
@@ -10,41 +10,103 @@ window.onload = () => {
     const deletaAllButton = document.querySelector(`#deletaAllButton`);
     const saveButton = document.querySelector(`#saveButton`);
 
-    //On Start
 
+    //Functions
+    const highlightTask = (element) => {
+        element.addEventListener(`click`, (event) => {
+            const isSelected = document.querySelector(`.selected`);
+            if (isSelected) {
+                isSelected.classList.remove(`selected`);
+            } else {
+                event.target.classList.add(`selected`)
+            };
+        })
+    }
+
+    const checkTask = (element) => {
+        element.addEventListener(`dblclick`, (event) => {
+            if (event.target.classList.contains(`concluedTask`)) {
+                event.target.classList.remove(`concluedTask`);
+            } else {
+                event.target.classList.add(`concluedTask`);
+            }
+        })
+    }
+
+    //On Start
+    if (localStorage.getItem(`myTaskList`) !== null) {
+        document.querySelector(`.taskList`).innerHTML = localStorage.getItem(`myTaskList`);
+        const array = document.querySelectorAll(`.taskList li`);
+
+        for (let index = 0; index < array.length; index += 1) {
+            highlightTask(array[index]);
+            checkTask(array[index]);
+        }
+    }
 
     //Buttons
     btnAddTask.addEventListener(`click`, () => {
         const task = inputText.value;
-        if (task === ``){
+        if (task === ``) {
             alert(`Insert a text before submit your task`);
-        }else {
-        const newElement = document.createElement(`li`);
-        newElement.innerText = task;
-        newElement.className = `singleTask`;
-        taskList.appendChild(newElement);
-        inputText.value = ``;
+        } else {
+            const newElement = document.createElement(`li`);
+            newElement.innerText = task;
+            newElement.className = `singleTask`;
+            taskList.appendChild(newElement);
+            inputText.value = ``;
 
-        newElement.addEventListener(`click`, (event) => {
-            const isSelected = document.querySelector(`.selected`);
-            if (isSelected){
-                isSelected.classList.remove(`selected`);
-            }
-            event.target.classList.add(`selected`);
-        })
-
-        newElement.addEventListener(`dblclick`, (event) => {
-            if (event.target.classList.contains(`concluedTask`)) {
-                event.target.classList.remove(`concluedTask`);
-            }
-            event.target.classList.add(`concluedTask`);
-        })
-    }
+            highlightTask(newElement);
+            checkTask(newElement);
+        }
     })
 
     deleteButton.addEventListener(`click`, () => {
         const isSelected = document.querySelector(`.selected`);
         isSelected.remove();
+    })
+
+    upperButton.addEventListener(`click`, () => {
+        const selectedTask = document.querySelector(`.selected`);
+        if (selectedTask.previousElementSibling === null) {
+            alert(`Already on the first position`);
+        } else {
+            const toChangeElement = selectedTask.previousElementSibling;
+            toChangeElement.parentNode.insertBefore(selectedTask, toChangeElement);
+        }
+    })
+
+    downButton.addEventListener(`click`, () => {
+        const selectedTask = document.querySelector(`.selected`);
+        if (selectedTask.nextElementSibling === null) {
+            alert(`Already on the last position`);
+        } else {
+            const selectedHtml = selectedTask.nextElementSibling;
+            selectedHtml.parentNode.insertBefore(selectedHtml, selectedTask);
+        }
+    })
+
+    deleteSelectedButton.addEventListener(`click`, () => {
+        const array = document.querySelectorAll(`.concluedTask`);
+        for (let index = 0; index < array.length; index += 1) {
+            array[index].remove();
+        }
+    })
+
+    deletaAllButton.addEventListener(`click`, () => {
+        const array = document.querySelectorAll(`.taskList li`);
+        for (let index = 0; index < array.length; index += 1) {
+            array[index].remove();
+        }
+    })
+
+    saveButton.addEventListener(`click`, () => {
+        const htmlSave = document.querySelector(`.taskList`).innerHTML;
+        localStorage.setItem(`myTaskList`, htmlSave);
+        const isSelected = document.querySelector(`.selected`)
+        if (isSelected) {
+            isSelected.classList.remove(`selected`);
+        }
     })
 
 }
